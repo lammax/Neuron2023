4cc1083d473dc63bb003b7680455b300a51923d9
Maksim Lamanskiy <mlamanskiy@ourgapps.com>
2023-01-04 19:46:07 +0300

    Initial Commit

---
+++ Neuron2023/Neuron2023App.swift
@@ -0,0 +1,17 @@
+//
+//  Neuron2023App.swift
+//  Neuron2023
+//
+//  Created by Maksim Lamanskiy on 04.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct Neuron2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
